04363c49ee11a9ff255f06b15f8ce036aceb0cc9
Stanley Zheng <szheng31@stuy.edu>
2024-10-30 14:33:28 -0500

    hi nirmal

--- app.py
+++ app.py
@@ -3,6 +3,29 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 import matplotlib.pyplot as plt
 
+import cv2
+
+def FrameCapture(path,name): 
+  
+    # Path to video file 
+    vidObj = cv2.VideoCapture(path) 
+  
+    # Used as counter variable 
+    count = 0
+  
+    # checks whether frames were extracted 
+    success = 1
+  
+    while success: 
+  
+        # vidObj object calls read 
+        # function extract frames 
+        success, image = vidObj.read() 
+  
+        # Saves the frames with frame-count 
+        cv2.imwrite(f"{name}frame%d.jpg" % count, image) 
+  
+        count += 1
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
